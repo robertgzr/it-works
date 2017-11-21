26cd24e81d29adf2405b926982f76be8f1c77db8
Robert Günzler <robertguenzler@kinvolk.io>
2017-11-21 18:25:35 +0100

    Fix plan.sh to install to use $pkg_prefix

--- habitat/plan.sh
+++ habitat/plan.sh
@@ -1,20 +1,20 @@
 pkg_name=it_works
 pkg_origin=robertgzr
 pkg_version="0.1.0"
-pkg_upstream_url=https://github.com/robertgzr/it_works
-pkg_maintainer="Robert Günzler <robertguenzler@kinvolk.io>"
 pkg_license=('MIT')
+pkg_maintainer="Robert Günzler <robertguenzler@kinvolk.io>"
+pkg_upstream_url=https://github.com/robertgzr/it_works
 pkg_bin_dirs=(bin)
+
+pkg_svc_run="it_works"
+pkg_svc_user=hab
 
 do_build() {
     return 0
 }
 
 do_install() {
-    mkdir -p bin/
-    cp artifacts/it_works_linux_amd64 bin/it_works
-}
-
-do_clean() {
-    rm -rf ./bin
+    mkdir -p $pkg_prefix/bin
+    cp $PLAN_CONTEXT/../artifacts/it_works_linux_amd64 $pkg_prefix/bin/it_works
+    chmod +x $pkg_prefix/bin/it_works
 }
