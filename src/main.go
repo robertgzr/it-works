ee03a6f4a52f95f3b5cf0fc9db7674df89332d1e
Robert Günzler <robertguenzler@kinvolk.io>
2017-12-07 16:41:44 +0100

    Update code and binary
    Add flags for port and custom response message
    Add logging

--- src/main.go
+++ src/main.go
@@ -1,15 +1,40 @@
 package main
 
 import (
+	"flag"
 	"io"
+	"log"
 	"net/http"
+	"time"
 )
 
+const defaultMsg = "it works!"
+
+var (
+	port string
+	msg  string
+)
+
+func init() {
+	flag.StringVar(&port, "port", "8000", "port to serve the content")
+	flag.StringVar(&msg, "msg", defaultMsg, "message to respond with")
+}
+
 func hello(w http.ResponseWriter, r *http.Request) {
-	io.WriteString(w, "it works now!")
+	log.Printf("[%s] %q", r.Method, r.URL.Path)
+	io.WriteString(w, time.Now().Format(time.RFC1123Z))
+	io.WriteString(w, "\n\n")
+	io.WriteString(w, msg)
+	io.WriteString(w, "\n")
 }
 
 func main() {
+	flag.Parse()
+
 	http.HandleFunc("/", hello)
-	http.ListenAndServe(":8000", nil)
+
+	addr := ":" + port
+	log.Printf("serving content at %v", addr)
+
+	http.ListenAndServe(addr, nil)
 }
