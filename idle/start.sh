d4017b886f894f72d888df09a5ff5823ecb705aa
Robert Günzler <r@gnzler.io>
2020-07-03 14:27:49 +0200

    idle: fix STRESS_ENABLED check

--- idle/start.sh
+++ idle/start.sh
@@ -4,7 +4,7 @@ set -e
 trap '{ echo "==> SIGTERM, exiting"; exit 0; }' TERM
 
 while true ; do
-    if [ -n "$STRESS_ENABLED" ]; then
+    if [ -n "$STRESS_ENABLED" ] && [ ! "$STRESS_ENABLED" = "0" ]; then
         timeout="${STRESS_TIMEOUT:-30s}"
         echo "Stressing for ${timeout}..."
         stress-ng --timeout "${timeout}" ${STRESS_ARGS}
