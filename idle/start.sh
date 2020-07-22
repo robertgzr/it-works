d7cc5f8d4aea8e9a2edce0574c0c0ca82ae991b5
Robert Günzler <r@gnzler.io>
2020-07-22 14:45:57 +0200

    report SIGTERM

--- idle/start.sh
+++ idle/start.sh
@@ -1,7 +1,13 @@
 #!/bin/sh
 set -e
 
-trap '{ echo "==> SIGTERM, exiting"; exit 0; }' TERM
+trap '{ echo "==> SIGTERM, exiting"; date >> /data/cookie; exit 0; }' TERM
+
+if [ -f /data/cookie ]; then
+    echo "==> previous shutdown"
+    cat /data/cookie
+    echo "==> eof"
+fi
 
 while true ; do
     if [ -n "$STRESS_ENABLED" ] && [ ! "$STRESS_ENABLED" = "0" ]; then
